a8bf246c0085de4f51318cb82851612c6241de1a
Viacheslav Mikerov <SlavaMikerov@gmail.com>
2015-04-28 00:36:13 +0200

    Fix LrzMacCluster configurations

--- tests/LrzMacCluster/job.sh
+++ tests/LrzMacCluster/job.sh
@@ -5,26 +5,25 @@
 #SBATCH -D  /home/hpc/pr63so/ga39puw2/jobs/output
 #SBATCH --mail-type=end
 #SBATCH --mail-user=SlavaMikerov@gmail.com
-#SBATCH --time=05:00:00
+#SBATCH --time=24:00:00
 #SBATCH --get-user-env
-#SBATCH --clusters=snb
+#SBATCH --partition=snb
 #SBATCH --ntasks=1
 
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$HOME/Project
+bin=$DIR/.install/Release/bin
+conf=$DIR/tests/LrzMacCluster
 
-source $DIR/environment-configuration.sh
-
-bin=$DIR/../../.install/Release/bin
-conf=$DIR
-
-mpiexec -n 1 $bin/Fluid \
-  -o Turek2D1-Ifsfd-11-220x205-11 \
-  -e $conf/Petsc/Basic.conf \
-  -s $conf/Fluid/Turek2D1-Ifsfd-11-220x205-11.xml \
-  -p $conf/Precice/Turek2D.xml
+source $conf/environment-configuration.sh
 
 mpiexec -n 1 $bin/Fluid \
-  -o Turek2D2-Ifsfd-11-220x205-11 \
+  -o Turek2D1-Ifsfd-11-440x82-11 \
   -e $conf/Petsc/Basic.conf \
-  -s $conf/Fluid/Turek2D2-Ifsfd-11-220x205-11.xml \
-  -p $conf/Precice/Turek2D.xml
+  -s $conf/Fluid/Turek2D1-Ifsfd-11-440x82-11.xml \
+  -p $conf/Precice/Turek2DNoIb-440x82.xml
+
+mpiexec -n 1 $bin/Fluid \
+  -o Turek2D2-Ifsfd-11-440x82-11 \
+  -e $conf/Petsc/Basic.conf \
+  -s $conf/Fluid/Turek2D2-Ifsfd-11-440x82-11.xml \
+  -p $conf/Precice/Turek2DNoIb-440x82.xml
