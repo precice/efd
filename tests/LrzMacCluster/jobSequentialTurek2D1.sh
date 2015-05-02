a2deea25566bf3927e7ddf009a8f1da91fabc838
Viacheslav Mikerov <SlavaMikerov@gmail.com>
2015-05-03 01:39:16 +0200

    Update LrzMacCluster job scripts

---
+++ tests/LrzMacCluster/jobSequentialTurek2D1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#SBATCH -J SequentialTurek2D1
+#SBATCH -o /home/hpc/pr63so/ga39puw2/jobs/logs/SequentialTurek2D1.%j.%N.out
+#SBATCH -D  /home/hpc/pr63so/ga39puw2/jobs/output
+#SBATCH --mail-type=end
+#SBATCH --mail-user=SlavaMikerov@gmail.com
+#SBATCH --time=24:00:00
+#SBATCH --get-user-env
+#SBATCH --partition=snb
+#SBATCH --ntasks=1
+
+DIR=$HOME/Project
+bin=$DIR/.install/Release/bin
+conf=$DIR/tests/LrzMacCluster
+
+source $conf/environment-configuration.sh
+
+mpiexec -n 1 $bin/Fluid \
+  -o Turek2D1-Ifsfd-11-440x82-11 \
+  -e $conf/Petsc/Basic.conf \
+  -s $conf/Fluid/Turek2D1-Ifsfd-11-440x82-11.xml \
+  -p $conf/Precice/Turek2DNoIb-440x82.xml
--- tests/LrzMacCluster/jobSequentialTurek2D2.sh
+++ tests/LrzMacCluster/jobSequentialTurek2D2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH -J myjob
-#SBATCH -o /home/hpc/pr63so/ga39puw2/jobs/logs/myjob.%j.%N.out
+#SBATCH -J SequentialTurek2D2
+#SBATCH -o /home/hpc/pr63so/ga39puw2/jobs/logs/SequentialTurek2D2.%j.%N.out
 #SBATCH -D  /home/hpc/pr63so/ga39puw2/jobs/output
 #SBATCH --mail-type=end
 #SBATCH --mail-user=SlavaMikerov@gmail.com
@@ -15,12 +15,6 @@ bin=$DIR/.install/Release/bin
 conf=$DIR/tests/LrzMacCluster
 
 source $conf/environment-configuration.sh
-
-mpiexec -n 1 $bin/Fluid \
-  -o Turek2D1-Ifsfd-11-440x82-11 \
-  -e $conf/Petsc/Basic.conf \
-  -s $conf/Fluid/Turek2D1-Ifsfd-11-440x82-11.xml \
-  -p $conf/Precice/Turek2DNoIb-440x82.xml
 
 mpiexec -n 1 $bin/Fluid \
   -o Turek2D2-Ifsfd-11-440x82-11 \
