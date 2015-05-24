01e015b84d556b271127c689298919ea2d4a9324
Viacheslav Mikerov <SlavaMikerov@gmail.com>
2015-05-24 14:36:22 +0200

    Add Turek2D2 test to LrzMacCluster tests

---
+++ tests/LrzMacCluster/generateAndSubmitTurek2D2.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-1x1-220x41-\w+-Precice\w*" -t 2 -m 1 --with-server -p Turek2D-IbMapping-220x41 "$@"
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-1x1-440x82-\w+-Precice\w*" -t 2 -m 1 --with-server -p Turek2D-IbMapping-440x82 "$@"
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-1x1-220x41-Rbf\w*" -t 1 -p Turek2D-NoIbMapping-220x41 "$@"
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-1x1-440x82-Rbf\w*" -t 1 -p Turek2D-NoIbMapping-440x82 "$@"
+
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-8x2-440x82-\w+-Precice\w*" -t 17 -m 16 --with-server -p Turek2D-IbMapping-440x82 "$@"
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-8x2-440x82-Rbf\w*" -t 16 -p Turek2D-NoIbMapping-440x82 "$@"
+
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-8x4-880x164-\w+-Precice\w*" -t 33 -m 32 --with-server -p Turek2D-IbMapping-880x164 "$@"
+./generateAndSubmitJobs.py -d Fluid/ -f "Turek2D2-\w+-16x4-880x164-Rbf\w*" -t 64  -p Turek2D-NoIbMapping-880x164 "$@"
